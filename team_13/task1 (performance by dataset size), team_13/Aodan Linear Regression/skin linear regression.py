531be13b41791a864b9863d47886cec3117ae312
Aodan O Laoghaire <olaoghaa@tcd.ie>
2017-10-24 22:17:01 +0100

    Added skin

---
+++ team_13/task1 (performance by dataset size), team_13/Aodan Linear Regression/skin linear regression.py
@@ -0,0 +1,40 @@
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+files="skin.csv"
+set_sizes = [100,500,1000,5000,10000,50000,100000,500000,1000000,5000000,10000000,50000000,100000000]
+column_names = ["Feature 1","Feature 2", "Feature 3","Target"]
+
+nrows2 = set_sizes[9]
+dataframe = pd.read_csv(files,
+                             sep=',',header=0,names=column_names,usecols=[0,1,2,3],
+                             nrows =nrows2)
+
+
+
+X_train = dataframe.head(int(nrows2 * .7))
+Y_train = X_train.Target
+X_train = X_train[["Feature 1","Feature 2","Feature 3"]]
+X_test = dataframe.tail(int(nrows2 * .3))
+Y_test = X_test.Target
+X_test = X_test[["Feature 1", "Feature 2","Feature 3"]]
+
+
+regr = linear_model.LinearRegression()
+
+# Train the model using the training sets
+regr.fit(X_train, Y_train)
+
+# Make predictions using the testing set
+Y_tester_pred = regr.predict(X_test)
+# The coefficients
+print('Coefficients: \n', regr.coef_)
+# The mean squared error
+print("Mean squared error: %.2f"
+      % mean_squared_error(Y_test, Y_tester_pred))
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % r2_score(Y_test, Y_tester_pred))
+#X_test.head()
+#Y_test.head()
+#X_train.head(10000)
