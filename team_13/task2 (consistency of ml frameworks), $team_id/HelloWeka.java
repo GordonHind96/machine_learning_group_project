e5b2f8a9174d21881adf1d3540e180195525db87
Brian de Buiteach <debuiteb@tcd.ie>
2017-10-27 19:22:20 +0100

    new csv for skin, and new alogrithm to generate said csv file

--- team_13/task2 (consistency of ml frameworks), $team_id/HelloWeka.java
+++ team_13/task2 (consistency of ml frameworks), $team_id/HelloWeka.java
@@ -1,13 +1,20 @@
 package weka.api;
 import java.awt.List;
+
+//written by Brian de Buiteach
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import weka.*;
 import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
 import weka.core.converters.ConverterUtils.DataSource;
 
 
@@ -22,16 +29,26 @@ public class HelloWeka {
 
 	private void generateCsvFile(String fileName) {
 		FileWriter writer = null;
+		Collections.shuffle(fullList);
 		try{
 			writer = new FileWriter(fileName);
 
 			for(ArrayList<String> currentRowList : fullList){
 				for(String str : currentRowList){
-					writer.append(str);
+					if(str.equals("1")){
+						writer.append("0");
+					}
+					else if(str.equals("2")){
+						writer.append("1");
+					}
+					else{
+						writer.append(str);
+					}
 					writer.append(",");
 				}
 				writer.append("\n");
 			}
+
 			System.out.println("CSV FILE CREATED - MAKE SURE YOU CAN FIND IT MATE");
 
 		}catch (IOException e) {
@@ -45,6 +62,76 @@ public class HelloWeka {
 			}
 		}
 	}
+
+
+	private void splitCSV(String name1,String name2,double train,double test) {
+		FileWriter writer1 = null;
+		FileWriter writer2 = null;
+		int index = 0;
+		int len = fullList.size();
+		try{
+			writer1 = new FileWriter(name1);
+
+
+			while(index < (len * train) ){
+				ArrayList<String> curRow = fullList.get(index);
+				index ++;
+				for(String str : curRow){
+					writer1.append(str);
+					writer1.append(",");
+				}
+				writer1.append("\n");
+			}
+
+			System.out.println("1st one made - CSV FILE CREATED - MAKE SURE YOU CAN FIND IT MATE");
+
+		}catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				writer1.flush();
+				writer1.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// test data
+		try{
+			writer2 = new FileWriter(name2);
+
+
+
+			while(index < len  ){
+				ArrayList<String> curRow = fullList.get(index);
+				index ++;
+				for(String str : curRow){
+					writer2.append(str);
+					writer2.append(",");
+				}
+				writer2.append("\n");
+			}
+
+			System.out.println("2nd CSV FILE CREATed");
+
+		}catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				writer2.flush();
+				writer2.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+
+
+	}
+
+
+
 
 	public void readInData(String path){
 		try {
@@ -80,7 +167,7 @@ public class HelloWeka {
 						i++;
 
 				}
-				
+
 				if(fullList == null){
 					fullList =  new ArrayList<ArrayList<String> >();
 				}
@@ -117,12 +204,12 @@ public class HelloWeka {
 		HelloWeka h = new HelloWeka();
 		h.readInData(path);
 		h.generateCsvFile("skin.csv");
-		System.out.println("we done");
 
-		//		DataSource source = new DataSource(path);
-		//		Instances dataset = source.getDataSet();
-		//	System.out.println(dataset.toSummaryString());
+		//h.splitCSV("skin_train.csv", "skin_test.csv", .7, .3);
 
+
+		//CSVLoader loader = new CSVLoader();
+		
 	}
 
 }
